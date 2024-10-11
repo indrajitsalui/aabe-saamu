66547046f55b4dad1158cc9142a55ebc162b27e3
indrajit salui <indrajitsalui15@gmail.com>
2024-10-11 23:50:04 +0530

    revert proxy changes

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiService = axios.create({
-    baseURL: '' // Leave it empty to use the proxy
+    baseURL: process.env.REACT_APP_API_URL // Leave it empty to use the proxy
 });
 const getAuthToken = () => {
     const token = localStorage.getItem('authToken');
