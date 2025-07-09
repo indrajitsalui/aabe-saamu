89e48eada73768dd47d7603fa0a8eb021b1eec7c
indrajit salui <indrajitsalui15@gmail.com>
2025-07-09 23:22:15 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate, useLocation } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import A1Productions from './pages/A1Productions/A1Productions';
 import Advertisements from './pages/Advertisements/Advertisements';
@@ -11,51 +11,62 @@ import GuestHome from './pages/GuestHome/GuestHome';
 import TermsAndConditions from './pages/TermsAndConditions/TermsAndConditions';
 import PrivacyPolicy from './pages/PrivacyPolicy/PrivacyPolicy';
 import LiveTV from './pages/LiveTV/LiveTV';
-import Header from './components/Header/Header'; // <-- Make sure this import is here
+import Header from './components/Header/Header';
+
+const AppContent = ({ handleLogout, handleLogin }) => {
+  const location = useLocation();
+  const hideHeaderRoutes = ['/privacy', '/terms-and-conditions'];
+
+  return (
+    <div className="app-container">
+      {!hideHeaderRoutes.includes(location.pathname) && (
+        <Header onLogout={handleLogout} />
+      )}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            localStorage.getItem('authToken') ? (
+              <Navigate to="/home" replace />
+            ) : (
+              <GuestHome />
+            )
+          }
+        />
+        <Route path="/home" element={<Home />} />
+        <Route path="/a1productions" element={<A1Productions />} />
+        <Route path="/advertisements" element={<Advertisements />} />
+        <Route path="/login" element={<Login onLogin={handleLogin} />} />
+        <Route path="/player/:videoId" element={<Player />} />
+        <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+        <Route path="/livetv" element={<LiveTV />} />
+      </Routes>
+    </div>
+  );
+};
 
 function App() {
-    const [isLoggedIn, setIsLoggedIn] = useState(() => {
-        return !!localStorage.getItem('authToken');
-    });
+  const [isLoggedIn, setIsLoggedIn] = useState(() => {
+    return !!localStorage.getItem('authToken');
+  });
 
-    const handleLogin = (token) => {
-        localStorage.setItem('authToken', token);
-        setIsLoggedIn(true);
-    };
+  const handleLogin = (token) => {
+    localStorage.setItem('authToken', token);
+    setIsLoggedIn(true);
+  };
 
-    const handleLogout = () => {
-        localStorage.removeItem('authToken');
-        localStorage.removeItem('userData');
-        setIsLoggedIn(false);
-    };
+  const handleLogout = () => {
+    localStorage.removeItem('authToken');
+    localStorage.removeItem('userData');
+    setIsLoggedIn(false);
+  };
 
-    return (
-        <Router>
-            <div className="app-container">
-                <Header onLogout={handleLogout} />
-                <Routes>
-                    <Route
-                        path="/"
-                        element={
-                            localStorage.getItem('authToken') ? (
-                                <Navigate to="/home" replace />
-                            ) : (
-                                <GuestHome />
-                            )
-                        }
-                    />
-                    <Route path="/home" element={<Home />} />
-                    <Route path="/a1productions" element={<A1Productions />} />
-                    <Route path="/advertisements" element={<Advertisements />} />
-                    <Route path="/login" element={<Login onLogin={handleLogin} />} />
-                    <Route path="/player/:videoId" element={<Player />} />
-                    <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
-                    <Route path="/privacy" element={<PrivacyPolicy />} />
-                    <Route path="/livetv" element={<LiveTV />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <AppContent handleLogout={handleLogout} handleLogin={handleLogin} />
+    </Router>
+  );
 }
 
 export default App;
